6e16199f756e64035359e506c62231448fe2678d
Riccardo Agatea <riccardo.agatea@gmail.com>
2020-03-15 00:06:59 +0100

    feat: add model class to login activity/viewmodel

---
+++ app/src/main/java/com/gruppone/stalker/LoginModel.java
@@ -0,0 +1,25 @@
+package com.gruppone.stalker;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import org.json.JSONObject;
+
+public class LoginModel {
+  public void login(String email, String password) {
+    WebSingleton.getInstance(App.getAppContext())
+                .addToRequestQueue(new JsonObjectRequest(
+                  "https://pokeapi.co/api/v2/pokemon/ditto/", null,
+                  new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject jsonObject) {
+                      //TODO
+                    }
+                  }, new Response.ErrorListener() {
+                  @Override
+                  public void onErrorResponse(VolleyError volleyError) {
+                    //TODO
+                  }
+                }));
+  }
+}
