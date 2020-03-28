dc7a0549f1e32defe154626cc41ec1917dc23f30
Riccardo Agatea <riccardo.agatea@gmail.com>
2020-03-28 18:39:37 +0100

    style: format code

--- app/src/main/java/com/gruppone/stalker/MainPageViewModel.java
+++ app/src/main/java/com/gruppone/stalker/MainPageViewModel.java
@@ -3,5 +3,5 @@ package com.gruppone.stalker;
 import androidx.lifecycle.ViewModel;
 
 public class MainPageViewModel extends ViewModel {
-  String[] organizationDataSet = { "Imola Informatica", "Università degli studi di Padova", "Zucchetti", "Alphabet Inc.", "Organizzazione 1", "Organizzazione 2", "Organizzazione 3", "Organizzazione 4", "Organizzazione 5", "Organizzazione 6", "Organizzazione 7", "Organizzazione 8", "Organizzazione 9", "Organizzazione 10" };
+  String[] organizationDataSet = {"Imola Informatica", "Università degli studi di Padova", "Zucchetti", "Alphabet Inc.", "Organizzazione 1", "Organizzazione 2", "Organizzazione 3", "Organizzazione 4", "Organizzazione 5", "Organizzazione 6", "Organizzazione 7", "Organizzazione 8", "Organizzazione 9", "Organizzazione 10"};
 }
