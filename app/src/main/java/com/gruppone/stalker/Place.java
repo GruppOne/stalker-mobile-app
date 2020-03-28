33caf120016976d6e631383eba759f6e6117ab2d
tobiaapolloni <tobia.apolloni@gmail.com>
2020-03-28 15:28:13 +0100

    feat: add Place class

---
+++ app/src/main/java/com/gruppone/stalker/Place.java
@@ -0,0 +1,19 @@
+package com.gruppone.stalker;
+
+import android.util.Pair;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Place {
+  private List<Pair<Double, Double>> polyLine;
+
+  public List<Pair<Double, Double>> GetPolyLine() {
+    return polyLine;
+  }
+
+  //to implement
+  public Boolean IsInside(Pair<Double, Double> point) {
+    return true;
+  }
+}
