0932e88b72b567e50ed3fe6e1d60e67b9df73d5e
Riccardo Agatea <riccardo.agatea@gmail.com>
2020-03-28 18:23:16 +0100

    feat: add check for permissions in base activity

--- app/src/main/java/com/gruppone/stalker/StalkerActivity.java
+++ app/src/main/java/com/gruppone/stalker/StalkerActivity.java
@@ -1,16 +1,15 @@
 package com.gruppone.stalker;
 
-import android.app.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class StalkerActivity extends AppCompatActivity {
-  void checkPermissions(Activity activity){
-
+  void checkPermissions() {
+    LocationNotifier.checkPermissions(this);
   }
 
   @Override
   protected void onResume() {
     super.onResume();
-    checkPermissions(this);
+    checkPermissions();
   }
 }
