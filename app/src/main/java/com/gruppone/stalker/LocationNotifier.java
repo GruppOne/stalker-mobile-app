d5715d19b42ea8e2044e34aaa130dcc7f6d21568
Riccardo Agatea <riccardo.agatea@gmail.com>
2020-03-28 17:02:26 +0100

    refactor: move location setup code into LocationNotifier

--- app/src/main/java/com/gruppone/stalker/LocationNotifier.java
+++ app/src/main/java/com/gruppone/stalker/LocationNotifier.java
@@ -1,16 +1,23 @@
 package com.gruppone.stalker;
 
+import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.location.Location;
+import android.content.IntentSender;
 import android.os.Looper;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.core.app.JobIntentService;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.*;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class LocationNotifier extends JobIntentService {
   static int JOB_ID = 1000;
+
+  private static LocationRequest locationRequest;
 
   public static void enqueue(Context ctx, Intent work) {
     enqueueWork(ctx, LocationNotifier.class, JOB_ID, work);
@@ -21,5 +28,67 @@ public class LocationNotifier extends JobIntentService {
     intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
     intent.setComponent(new ComponentName(this, Display.class));
     startActivity(intent);
+  }
+
+  static void startLocationUpdates() {
+    FusedLocationProviderClient locationProviderClient = LocationServices.getFusedLocationProviderClient(
+      App.getAppContext());
+
+    locationProviderClient.requestLocationUpdates(locationRequest,
+                                                  new LocationCallback() {
+                                                    @Override
+                                                    public void onLocationAvailability(LocationAvailability locationAvailability) {
+
+                                                    }
+
+                                                    @Override
+                                                    public void onLocationResult(LocationResult locationResult) {
+                                                      Intent intent = new Intent();
+                                                      intent.putExtra(
+                                                        "lastLocation",
+                                                        locationResult.getLastLocation());
+                                                      LocationNotifier.enqueue(
+                                                        App.getAppContext(),
+                                                        intent);
+                                                    }
+                                                  }, Looper.getMainLooper());
+  }
+
+  static void checkPermissions(Activity activity) {
+    if (locationRequest == null) {
+      locationRequest = LocationRequest.create();
+      locationRequest.setInterval(300000);
+      locationRequest.setFastestInterval(150000);
+      locationRequest.setPriority(
+        LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+    }
+
+    LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(
+      locationRequest);
+
+    LocationServices.getSettingsClient(activity)
+                    .checkLocationSettings(builder.build())
+                    .addOnSuccessListener(
+                      new OnSuccessListener<LocationSettingsResponse>() {
+                        @Override
+                        public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                        }
+                      })
+                    .addOnFailureListener(new OnFailureListener() {
+                      @Override
+                      public void onFailure(@NonNull Exception e) {
+                        if (e instanceof ResolvableApiException) {
+                          try {
+                            ResolvableApiException resolvableApiException = (ResolvableApiException) e;
+                            resolvableApiException.startResolutionForResult(
+                              activity, 1010);
+                          } catch (IntentSender.SendIntentException exc) {
+                            Toast.makeText(activity, "Nouse",
+                                           Toast.LENGTH_SHORT)
+                                 .show();
+                          }
+                        }
+                      }
+                    });
   }
 }
