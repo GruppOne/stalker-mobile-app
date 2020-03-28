9655637e152c2853045ee197b14f9c557818f930
Riccardo Agatea <riccardo.agatea@gmail.com>
2020-03-28 18:59:28 +0100

    refactor(loginactivity): use lambda instead of listener

--- app/src/main/java/com/gruppone/stalker/LoginActivity.java
+++ app/src/main/java/com/gruppone/stalker/LoginActivity.java
@@ -1,7 +1,6 @@
 package com.gruppone.stalker;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.EditText;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -15,16 +14,10 @@ public class LoginActivity extends StalkerActivity {
 
     viewModel = new ViewModelProvider(this).get(LoginViewModel.class);
 
-    findViewById(R.id.loginButton).setOnClickListener(
-      new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-          viewModel.login(
-            ((EditText) findViewById(R.id.emailEditText)).getText()
-                                                         .toString(),
-            ((EditText) findViewById(R.id.passwordEditText)).getText()
-                                                            .toString());
-        }
-      });
+    findViewById(R.id.loginButton).setOnClickListener(v -> viewModel.login(
+      ((EditText) findViewById(R.id.emailEditText)).getText()
+                                                   .toString(),
+      ((EditText) findViewById(R.id.passwordEditText)).getText()
+                                                      .toString()));
   }
 }
