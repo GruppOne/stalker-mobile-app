225b14676e7716c9a6315a81ddb0d17647bc6110
tobiaapolloni <tobia.apolloni@gmail.com>
2020-03-29 16:28:35 +0200

    feat(login): add MVVM implementation for Login

--- app/src/main/java/com/gruppone/stalker/LoginModel.java
+++ app/src/main/java/com/gruppone/stalker/LoginModel.java
@@ -1,25 +1,21 @@
 package com.gruppone.stalker;
 
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import org.json.JSONObject;
-
 public class LoginModel {
-  public void login(String email, String password) {
-    WebSingleton.getInstance()
-                .addToRequestQueue(new JsonObjectRequest(
-                  "https://pokeapi.co/api/v2/pokemon/ditto/", null,
-                  new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject jsonObject) {
-                      //TODO
-                    }
-                  }, new Response.ErrorListener() {
-                  @Override
-                  public void onErrorResponse(VolleyError volleyError) {
-                    //TODO
-                  }
-                }));
+  public Boolean login(String email, String password) {
+    return true;
+    //WebSingleton.getInstance(App.getAppContext())
+    //            .addToRequestQueue(new JsonObjectRequest(
+    //              "https://pokeapi.co/api/v2/pokemon/ditto/", null,
+    //              new Response.Listener<JSONObject>() {
+    //                @Override
+    //                public void onResponse(JSONObject jsonObject) {
+    //                  //TODO
+    //                }
+    //              }, new Response.ErrorListener() {
+    //              @Override
+    //              public void onErrorResponse(VolleyError volleyError) {
+    //                //TODO
+    //              }
+    //            }));
   }
 }
