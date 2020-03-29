c0b77e4492d92aea4c944b0971fc8f3531b71557
tobiaapolloni <tobia.apolloni@gmail.com>
2020-03-29 16:24:49 +0200

    feat(mainpage): add MVVM implementation for MainPage

--- app/src/main/java/com/gruppone/stalker/MainPageActivity.java
+++ app/src/main/java/com/gruppone/stalker/MainPageActivity.java
@@ -1,9 +1,12 @@
 package com.gruppone.stalker;
 
 import android.os.Bundle;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainPageActivity extends StalkerActivity {
   private RecyclerView recyclerView;
@@ -18,11 +21,19 @@ public class MainPageActivity extends StalkerActivity {
 
     viewModel = new ViewModelProvider(this).get(MainPageViewModel.class);
 
+    final Observer<ArrayList<String>> organizationDataSetObserver = newOrganizationDataSet -> mAdapter.notifyDataSetChanged();
+
+    viewModel.getOrganizationDataSet()
+             .observe(this, organizationDataSetObserver);
+
     recyclerView = findViewById(R.id.organizationRecyclerView);
     recyclerView.setHasFixedSize(true);
     layoutManager = new LinearLayoutManager(this);
     recyclerView.setLayoutManager(layoutManager);
-    mAdapter = new MyAdapter(viewModel.organizationDataSet);
+    mAdapter = new OrganizationListAdapter(viewModel.getOrganizationDataSet()
+                                                    .getValue());
+
+    mAdapter.notifyDataSetChanged();
     recyclerView.setAdapter(mAdapter);
   }
 }
--- app/src/main/java/com/gruppone/stalker/MainPageViewModel.java
+++ app/src/main/java/com/gruppone/stalker/MainPageViewModel.java
@@ -1,7 +1,14 @@
 package com.gruppone.stalker;
 
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
 
+import java.util.ArrayList;
+
 public class MainPageViewModel extends ViewModel {
-  String[] organizationDataSet = {"Imola Informatica", "Università degli studi di Padova", "Zucchetti", "Alphabet Inc.", "Organizzazione 1", "Organizzazione 2", "Organizzazione 3", "Organizzazione 4", "Organizzazione 5", "Organizzazione 6", "Organizzazione 7", "Organizzazione 8", "Organizzazione 9", "Organizzazione 10"};
+  private MainPageModel model = new MainPageModel();
+
+  public LiveData<ArrayList<String>> getOrganizationDataSet() {
+    return model.getOrganizationDataSet();
+  }
 }
