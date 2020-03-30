0226c38f01a648e1a7f388537137ed0d19b80cba
Riccardo Agatea <riccardo.agatea@gmail.com>
2020-03-30 18:16:09 +0200

    test: add empty test classes for all classes

---
+++ app/src/androidTest/java/com/gruppone/stalker/MainPageActivityTest.java
@@ -0,0 +1,17 @@
+package com.gruppone.stalker;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MainPageActivityTest {
+
+  @Test
+  public void onCreate() {
+    //Arrange
+
+    //Act
+
+    //Assert
+  }
+}
---
+++ app/src/test/java/com/gruppone/stalker/CurrentSessionSingletonTest.java
@@ -0,0 +1,26 @@
+package com.gruppone.stalker;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CurrentSessionSingletonTest {
+
+  @Test
+  public void zeroOrganizations() {
+    //Arrange
+
+    //Act
+
+    //Assert
+  }
+
+  @Test
+  public void getInstance() {
+    //Arrange
+
+    //Act
+
+    //Assert
+  }
+}
