8574ec27c6974da23eb65c65d13fd70bbb89306e
Riccardo Agatea <riccardo.agatea@gmail.com>
2020-03-31 23:03:51 +0200

    feat: add getter for place id

--- app/src/main/java/com/gruppone/stalker/Place.java
+++ app/src/main/java/com/gruppone/stalker/Place.java
@@ -1,14 +1,19 @@
 package com.gruppone.stalker;
 
 import android.util.Pair;
-
 import java.util.List;
 
 public class Place {
+
+  private int id;
   private List<Pair<Double, Double>> polyLine;
 
   public List<Pair<Double, Double>> getPolyLine() {
     return polyLine;
+  }
+
+  public int getId() {
+    return id;
   }
 
   //to implement
