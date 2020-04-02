c467fc9eb226b755e7be258d22b630034ac9d6c0
Riccardo Agatea <riccardo.agatea@gmail.com>
2020-04-02 14:23:23 +0200

    fix: set server port to 11111

--- app/src/main/java/com/gruppone/stalker/WebSingleton.java
+++ app/src/main/java/com/gruppone/stalker/WebSingleton.java
@@ -10,7 +10,7 @@ public class WebSingleton {
 
   private static WebSingleton instance;
   private RequestQueue requestQueue;
-  private String serverUrl = "10.0.2.2:8080";
+  private String serverUrl = "10.0.2.2:11111";
 
   private WebSingleton() {
     requestQueue = getRequestQueue();
