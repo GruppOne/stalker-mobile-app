b983a0da2d2e337f5d6eb3bf5197ee5d55659d01
Riccardo Agatea <riccardo.agatea@gmail.com>
2020-04-02 18:16:37 +0200

    refactor: use lombok for getters in existing classes

--- app/src/main/java/com/gruppone/stalker/CurrentSessionSingleton.java
+++ app/src/main/java/com/gruppone/stalker/CurrentSessionSingleton.java
@@ -2,12 +2,15 @@ package com.gruppone.stalker;
 
 import androidx.lifecycle.MutableLiveData;
 import java.util.List;
+import lombok.Getter;
 
 public class CurrentSessionSingleton {
 
   private static CurrentSessionSingleton instance;
 
+  @Getter
   private User loggedUser;
+
   private MutableLiveData<List<Organization>> organizations = new MutableLiveData<>();
 
   private CurrentSessionSingleton() {
@@ -21,10 +24,6 @@ public class CurrentSessionSingleton {
 
   boolean zeroOrganizations() {
     return organizations.getValue() == null || organizations.getValue().isEmpty();
-  }
-
-  User getLoggedUser() {
-    return loggedUser;
   }
 
   public static synchronized CurrentSessionSingleton getInstance() {
