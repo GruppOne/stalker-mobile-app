c7339d77fc7400bfa1efba9e324863e8c65d1202
Riccardo Agatea <riccardo.agatea@gmail.com>
2020-04-04 23:25:54 +0200

    feat: implement organization list reception

--- app/src/main/java/com/gruppone/stalker/MainPageModel.java
+++ app/src/main/java/com/gruppone/stalker/MainPageModel.java
@@ -1,12 +1,27 @@
 package com.gruppone.stalker;
 
 import androidx.lifecycle.LiveData;
+import java.util.ArrayList;
 import java.util.List;
+import org.json.JSONException;
 
 public class MainPageModel {
 
   void loadOrganizations() {
-    WebSingleton.getInstance().getOrganizationList();
+    WebSingleton.getInstance().getOrganizationList(jsonArray -> {
+      List<Organization> organizations = new ArrayList<>();
+
+      try {
+        for (int i = 0; i < jsonArray.length(); ++i) {
+          organizations
+            .add(new Organization(jsonArray.getJSONObject(i)));
+        }
+      } catch (JSONException e) {
+        throw new RuntimeException(e);
+      }
+
+      CurrentSessionSingleton.getInstance().setOrganizations(organizations);
+    }, null);
   }
 
   public LiveData<List<Organization>> getOrgsLiveData() {
