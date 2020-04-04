5b2c9e8d15c8190f7d88047d5e35e276456b6e03
tobiaapolloni <tobia.apolloni@gmail.com>
2020-04-04 15:54:40 +0200

    refactor: fix formatting and imports on PlaceTest class

--- app/src/test/java/com/gruppone/stalker/PlaceTest.java
+++ app/src/test/java/com/gruppone/stalker/PlaceTest.java
@@ -1,9 +1,8 @@
 package com.gruppone.stalker;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
+
 import android.util.Pair;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,18 +13,18 @@ public class PlaceTest {
   @Test
   public void isInside() {
     //Arrange
-    List<Pair<Double,Double>> pairList = new ArrayList<>();
-    pairList.add(new Pair<>(0.0,0.0));
-    pairList.add(new Pair<>(1.0,0.0));
-    pairList.add(new Pair<>(1.0,1.0));
-    pairList.add(new Pair<>(0.0,1.0));
+    List<Pair<Double, Double>> pairList = new ArrayList<>();
+    pairList.add(new Pair<>(0.0, 0.0));
+    pairList.add(new Pair<>(1.0, 0.0));
+    pairList.add(new Pair<>(1.0, 1.0));
+    pairList.add(new Pair<>(0.0, 1.0));
 
     Place instance = new Place(pairList);
 
     //Act
-    Boolean firstInside = instance.isInside(new Pair<>(0.5,0.5));
-    Boolean secondInside = instance.isInside(new Pair<>(5.0,5.0));
-    Boolean thirdInside = instance.isInside(new Pair<>(0.0,0.0));
+    Boolean firstInside = instance.isInside(new Pair<>(0.5, 0.5));
+    Boolean secondInside = instance.isInside(new Pair<>(5.0, 5.0));
+    Boolean thirdInside = instance.isInside(new Pair<>(0.0, 0.0));
 
     //Assert
     assertTrue(firstInside);
