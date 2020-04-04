a3388f5be961185651e9c9b8188b2aeeaa040f56
tobiaapolloni <tobia.apolloni@gmail.com>
2020-04-04 16:41:27 +0200

    refactor: fix naming and test structure for CurrentSessionSingletonTest class

--- app/src/test/java/com/gruppone/stalker/CurrentSessionSingletonTest.java
+++ app/src/test/java/com/gruppone/stalker/CurrentSessionSingletonTest.java
@@ -13,62 +13,123 @@ import org.junit.Test;
 public class CurrentSessionSingletonTest {
 
   @Test
-  public void zeroOrganizations() {
+  public void zeroOrganizations_emptyOrganizationArray() {
     //Arrange
-    CurrentSessionSingleton first = CurrentSessionSingleton.getInstance();
-    CurrentSessionSingleton second = CurrentSessionSingleton.getInstance();
-    CurrentSessionSingleton third = CurrentSessionSingleton.getInstance();
-    List<Organization> organizationList = new ArrayList<>();
-    organizationList.add(new Organization(1, "test", null));
+    final CurrentSessionSingleton instance = CurrentSessionSingleton.getInstance();
 
     //Act
-    first.setOrganizations(new ArrayList<>());
-    second.setOrganizations(null);
-    third.setOrganizations(organizationList);
+    instance.setOrganizations(new ArrayList<>());
 
     //Assert
-    assertTrue(first.zeroOrganizations());
-    assertTrue(second.zeroOrganizations());
-    assertFalse(third.zeroOrganizations());
+    assertTrue(instance.zeroOrganizations());
   }
 
   @Test
-  public void getInsideId() {
+  public void zeroOrganizations_nullOrganizationArray() {
     //Arrange
-    CurrentSessionSingleton first = CurrentSessionSingleton.getInstance();
-    CurrentSessionSingleton second = CurrentSessionSingleton.getInstance();
-    CurrentSessionSingleton third = CurrentSessionSingleton.getInstance();
+    final CurrentSessionSingleton instance = CurrentSessionSingleton.getInstance();
 
-    List<Pair<Double, Double>> pairList = new ArrayList<>();
+    //Act
+    instance.setOrganizations(null);
+
+    //Assert
+    assertTrue(instance.zeroOrganizations());
+  }
+
+  @Test
+  public void zeroOrganizations_notEmptyOrganizationArray() {
+    //Arrange
+    final CurrentSessionSingleton instance = CurrentSessionSingleton.getInstance();
+    final List<Organization> organizationList = new ArrayList<>();
+    organizationList.add(new Organization(1, "test", null));
+
+    //Act
+    instance.setOrganizations(organizationList);
+
+    //Assert
+    assertFalse(instance.zeroOrganizations());
+  }
+
+  @Test
+  public void getInsideId_insidePoint() {
+    //Arrange
+    final CurrentSessionSingleton instance = CurrentSessionSingleton.getInstance();
+
+    final List<Pair<Double, Double>> pairList = new ArrayList<>();
     pairList.add(new Pair<>(0.0, 0.0));
     pairList.add(new Pair<>(1.0, 0.0));
     pairList.add(new Pair<>(1.0, 1.0));
     pairList.add(new Pair<>(0.0, 1.0));
 
-    List<Place> placeList = new ArrayList<>();
+    final List<Place> placeList = new ArrayList<>();
     placeList.add(new Place(pairList));
 
-    List<Organization> organizationList = new ArrayList<>();
+    final List<Organization> organizationList = new ArrayList<>();
     organizationList.add(new Organization(1, "test", placeList));
 
-    first.setOrganizations(organizationList);
-    third.setOrganizations(organizationList);
+    instance.setOrganizations(organizationList);
 
     //Act
-    List<Integer> firstinsideList = first.getInsideId(new Pair<>(0.5, 0.5));
-    List<Integer> secondinsideList = second.getInsideId(new Pair<>(0.5, 0.5));
-    List<Integer> thirdinsideList = third.getInsideId(new Pair<>(5.0, 5.0));
+    final List<Integer> insideList = instance.getInsideId(new Pair<>(0.5, 0.5));
 
     //Assert
-    assertNotEquals(null, firstinsideList);
-    assertNotEquals(0, firstinsideList.size());
-    assertNotEquals(null, secondinsideList);
-    assertEquals(0, secondinsideList.size());
-    assertEquals(0, thirdinsideList.size());
+    assertNotEquals(null, insideList);
+    assertNotEquals(0, insideList.size());
   }
 
   @Test
-  public void getInstance() {
+  public void getInsideId_nullOrganizationList() {
+    //Arrange
+    final CurrentSessionSingleton instance = CurrentSessionSingleton.getInstance();
+
+    //Act
+    final List<Integer> insideList = instance.getInsideId(new Pair<>(0.5, 0.5));
+
+    //Assert
+    assertNotEquals(null, insideList);
+    assertEquals(0, insideList.size());
+  }
+
+  @Test
+  public void getInsideId_outsidePoint() {
+    //Arrange
+    final CurrentSessionSingleton instance = CurrentSessionSingleton.getInstance();
+
+    final List<Pair<Double, Double>> pairList = new ArrayList<>();
+    pairList.add(new Pair<>(0.0, 0.0));
+    pairList.add(new Pair<>(1.0, 0.0));
+    pairList.add(new Pair<>(1.0, 1.0));
+    pairList.add(new Pair<>(0.0, 1.0));
+
+    final List<Place> placeList = new ArrayList<>();
+    placeList.add(new Place(pairList));
+
+    final List<Organization> organizationList = new ArrayList<>();
+    organizationList.add(new Organization(1, "test", placeList));
+
+    instance.setOrganizations(organizationList);
+
+    //Act
+    final List<Integer> insideList = instance.getInsideId(new Pair<>(5.0, 5.0));
+
+    //Assert
+    assertNotEquals(null, insideList);
+    assertEquals(0, insideList.size());
+  }
+
+  @Test
+  public void getInstance_notNull() {
+    //Arrange
+    CurrentSessionSingleton instance = CurrentSessionSingleton.getInstance();
+
+    //Act
+
+    //Assert
+    assertNotEquals(null, instance);
+  }
+
+  @Test
+  public void getInstance_instanceEqual() {
     //Arrange
     CurrentSessionSingleton first = CurrentSessionSingleton.getInstance();
     CurrentSessionSingleton second = CurrentSessionSingleton.getInstance();
@@ -76,7 +137,6 @@ public class CurrentSessionSingletonTest {
     //Act
 
     //Assert
-    assertNotEquals(null, first);
     assertEquals(first, second);
   }
 }
