b9c99e493dca36ba2300b0c5ffe777a9a0d43f48
Riccardo Agatea <riccardo.agatea@gmail.com>
2020-04-04 23:23:58 +0200

    feat: add Marcator projection helper functions
    The locations handled by the location services and those returned by the server are in longitude and
    latitude (degrees), but the algorithm we chose to check whether or not the user location is inside a
    place is based in vectorial geometry, and so needs a carthesian plane to work. Therefore the need
    for Marcator.

--- app/src/main/java/com/gruppone/stalker/Point.java
+++ app/src/main/java/com/gruppone/stalker/Point.java
@@ -1,9 +1,44 @@
 package com.gruppone.stalker;
 
+import lombok.AllArgsConstructor;
 import lombok.Value;
 
 @Value
+@AllArgsConstructor
 public class Point {
 
+
   double x, y;
+
+  public static Point buildFromDegrees(double longitude, double latitude) {
+    return new Point(xMarcator(longitude), yMarcator(latitude));
+  }
+
+  //constants and private functions to convert longitude and latitude into a cartesian system on the plane
+  private static final double EARTH_EQUAT_RADIUS = 6378137.0;
+  private static final double EARTH_POLAR_RADIUS = 6356752.3142;
+
+  private static double xMarcator(double longitude) {
+    return EARTH_EQUAT_RADIUS * Math.toRadians(longitude);
+  }
+
+  private static double yMarcator(double latitude) {
+    //Above 89.5° or below -89.5° the projection breaks
+    latitude = Math.min(Math.max(latitude, -89.5), 89.5);
+
+    //Marcator projection:
+    double earthDimensionalRateNormalized =
+      1.0 - Math.pow(EARTH_POLAR_RADIUS / EARTH_EQUAT_RADIUS, 2);
+
+    double latitudeOnEarthProj = Math.sqrt(earthDimensionalRateNormalized) *
+      Math.sin(Math.toRadians(latitude));
+
+    latitudeOnEarthProj = Math.pow(((1.0 - latitudeOnEarthProj) / (1.0 + latitudeOnEarthProj)),
+      0.5 * Math.sqrt(earthDimensionalRateNormalized));
+
+    double inputOnEarthProjNormalized =
+      Math.tan(0.5 * ((Math.PI * 0.5) - Math.toRadians(latitude))) / latitudeOnEarthProj;
+
+    return (-1) * EARTH_EQUAT_RADIUS * Math.log(inputOnEarthProjNormalized);
+  }
 }
