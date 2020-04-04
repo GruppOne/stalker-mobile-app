a8abe51daf73f7e738773ce5a0dd7aef5fc168ec
Riccardo Agatea <riccardo.agatea@gmail.com>
2020-04-04 23:24:50 +0200

    feat: add constructors for JSON parsing

--- app/src/main/java/com/gruppone/stalker/Organization.java
+++ app/src/main/java/com/gruppone/stalker/Organization.java
@@ -1,9 +1,13 @@
 package com.gruppone.stalker;
 
+import java.util.ArrayList;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 @EqualsAndHashCode
 @AllArgsConstructor
@@ -16,6 +20,22 @@ public class Organization {
   private String name;
 
   private List<Place> places;
+
+  public Organization(JSONObject jsonOrg) {
+    try {
+      id = jsonOrg.getInt("id");
+      name = jsonOrg.getString("name");
+      places = new ArrayList<>();
+
+      JSONArray jsonPlaces = jsonOrg.getJSONArray("places");
+
+      for (int i = 0; i < jsonPlaces.length(); ++i) {
+        places.add(new Place(jsonPlaces.getJSONObject(i)));
+      }
+    } catch (JSONException e) {
+      throw new RuntimeException(e);
+    }
+  }
 
   public boolean isInside(Point point) {
     boolean ret = false;
--- app/src/main/java/com/gruppone/stalker/Place.java
+++ app/src/main/java/com/gruppone/stalker/Place.java
@@ -1,9 +1,13 @@
 package com.gruppone.stalker;
 
+import java.util.ArrayList;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 @EqualsAndHashCode
 @AllArgsConstructor
@@ -14,6 +18,24 @@ public class Place {
 
   @Getter
   private List<Point> polyLine;
+
+  public Place(JSONObject jsonPlace) {
+    try {
+      id = jsonPlace.getInt("id");
+      polyLine = new ArrayList<>();
+
+      JSONArray jsonPolyLine = jsonPlace.getJSONArray("polyline");
+
+      for (int i = 1; i < jsonPolyLine.length(); ++i) {
+        JSONObject jsonPoint = jsonPolyLine.getJSONObject(i);
+
+        polyLine.add(Point
+          .buildFromDegrees(jsonPoint.getDouble("longitude"), jsonPoint.getDouble("latitude")));
+      }
+    } catch (JSONException e) {
+      throw new RuntimeException(e);
+    }
+  }
 
   public boolean isInside(Point point) {
     double xMin = Double.MAX_VALUE;
