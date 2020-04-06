c6a8926be43888fa22c92538dc8ca776c76db0bb
Riccardo Agatea <riccardo.agatea@gmail.com>
2020-04-06 15:55:14 +0200

    refactor: split interface to Google API from LocationNotifier

--- app/src/main/java/com/gruppone/stalker/LocationNotifier.java
+++ app/src/main/java/com/gruppone/stalker/LocationNotifier.java
@@ -1,29 +1,16 @@
 package com.gruppone.stalker;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentSender;
-import android.os.Looper;
-import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.core.app.JobIntentService;
-import com.google.android.gms.common.api.ResolvableApiException;
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationAvailability;
-import com.google.android.gms.location.LocationCallback;
-import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.LocationSettingsRequest;
 
 public class LocationNotifier extends JobIntentService {
 
   static int JOB_ID = 1000;
   private CurrentSessionSingleton currentSession = CurrentSessionSingleton.getInstance();
   private WebSingleton web = WebSingleton.getInstance();
-
-  private static LocationRequest locationRequest;
 
   public static void enqueue(Context ctx, Intent work) {
     enqueueWork(ctx, LocationNotifier.class, JOB_ID, work);
@@ -36,64 +23,5 @@ public class LocationNotifier extends JobIntentService {
       location.getLastLocation().getLatitude());
     web.locationUpdateInside(currentSession.getLoggedUser().getId(),
       currentSession.getInsidePlaces(point));
-  }
-
-  //TODO Move these functions in a separate interface
-  static void startLocationUpdates(Activity activity) {
-    checkPermissions(activity);
-
-    FusedLocationProviderClient locationProviderClient = LocationServices
-      .getFusedLocationProviderClient(
-        App.getAppContext());
-
-    locationProviderClient.requestLocationUpdates(locationRequest,
-      new LocationCallback() {
-        @Override
-        public void onLocationAvailability(LocationAvailability locationAvailability) {
-
-        }
-
-        @Override
-        public void onLocationResult(LocationResult locationResult) {
-          Intent intent = new Intent();
-          intent.putExtra(
-            "lastLocation",
-            locationResult.getLastLocation());
-          LocationNotifier.enqueue(
-            App.getAppContext(),
-            intent);
-        }
-      }, Looper.getMainLooper());
-  }
-
-  static void checkPermissions(Activity activity) {
-    if (locationRequest == null) {
-      locationRequest = LocationRequest.create();
-      locationRequest.setInterval(300000);
-      locationRequest.setFastestInterval(150000);
-      locationRequest.setPriority(
-        LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
-    }
-
-    LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
-      .addLocationRequest(
-        locationRequest);
-
-    LocationServices.getSettingsClient(activity)
-      .checkLocationSettings(builder.build())
-      .addOnSuccessListener(locationSettingsResponse -> {
-      })
-      .addOnFailureListener(e -> {
-        if (e instanceof ResolvableApiException) {
-          try {
-            ResolvableApiException resolvableApiException = (ResolvableApiException) e;
-            resolvableApiException.startResolutionForResult(
-              activity, 1010);
-          } catch (IntentSender.SendIntentException exc) {
-            Toast.makeText(activity, "Nouse", Toast.LENGTH_SHORT)
-              .show();
-          }
-        }
-      });
   }
 }
