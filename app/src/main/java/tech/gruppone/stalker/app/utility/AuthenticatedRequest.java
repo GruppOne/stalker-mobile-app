31d6fe8cd8a956aac794cbd3b518c81b2e6d3d0c
Riccardo Agatea <riccardo.agatea@gmail.com>
2020-05-03 19:10:33 +0200

    refactor: use decorator to handle request headers
    Finally the HeadersAdders interface, which was an abomination in the eyes of men and gods, has gone
    away, and with it our sins. In its place, an actual decorator has been established. May copious
    blessings come our way.

---
+++ app/src/main/java/tech/gruppone/stalker/app/utility/AuthenticatedRequest.java
@@ -0,0 +1,30 @@
+package tech.gruppone.stalker.app.utility;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import com.android.volley.AuthFailureError;
+import com.android.volley.Response;
+import com.android.volley.toolbox.JsonObjectRequest;
+import java.util.HashMap;
+import java.util.Map;
+import org.json.JSONObject;
+import tech.gruppone.stalker.app.BuildConfig;
+
+public class AuthenticatedRequest extends JsonObjectRequest {
+  public AuthenticatedRequest(
+      int method,
+      @NonNull String url,
+      @Nullable JSONObject jsonRequest,
+      @Nullable Response.Listener<JSONObject> listener,
+      @Nullable Response.ErrorListener errorListener) {
+    super(method, url, jsonRequest, listener, errorListener);
+  }
+
+  @NonNull
+  @Override
+  public Map<String, String> getHeaders() throws AuthFailureError {
+    Map<String, String> headers = new HashMap<>(super.getHeaders());
+    headers.put("STALKER-ADMIN-API-KEY", BuildConfig.ADMIN_API_KEY);
+    return headers;
+  }
+}
