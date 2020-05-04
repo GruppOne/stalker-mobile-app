becb07cf554a7c64cb5efb2a95738576e6f788bc
Riccardo Agatea <riccardo.agatea@gmail.com>
2020-05-04 19:16:19 +0200

    fix: move password hashing from model to viewmodel

--- app/src/main/java/tech/gruppone/stalker/app/viewmodel/LoginViewModel.java
+++ app/src/main/java/tech/gruppone/stalker/app/viewmodel/LoginViewModel.java
@@ -1,8 +1,10 @@
 package tech.gruppone.stalker.app.viewmodel;
 
-import android.annotation.SuppressLint;
 import androidx.annotation.NonNull;
 import androidx.lifecycle.ViewModel;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 import tech.gruppone.stalker.app.model.LoginModel;
 
 public class LoginViewModel extends ViewModel {
@@ -10,6 +12,16 @@ public class LoginViewModel extends ViewModel {
   private LoginModel model = new LoginModel();
 
   public void login(@NonNull String email, @NonNull String password) {
-    model.login(email, password);
+    String hashedPassword;
+
+    try {
+      MessageDigest messageDigest = MessageDigest.getInstance("SHA-512");
+
+      hashedPassword = Arrays.toString(messageDigest.digest(password.getBytes()));
+    } catch (NoSuchAlgorithmException e) {
+      throw new RuntimeException(e);
+    }
+    
+    model.login(email, hashedPassword);
   }
 }
