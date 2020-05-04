9fa1e0364e93369952d4bdbe18d188f3cba8c5f6
Riccardo Agatea <riccardo.agatea@gmail.com>
2020-05-04 14:05:25 +0200

    fix: add user fields

--- app/src/main/java/tech/gruppone/stalker/app/business/User.java
+++ app/src/main/java/tech/gruppone/stalker/app/business/User.java
@@ -1,9 +1,18 @@
 package tech.gruppone.stalker.app.business;
 
+import androidx.annotation.NonNull;
+import lombok.Setter;
 import lombok.Value;
+import lombok.experimental.NonFinal;
 
 @Value
 public class User {
 
-  int id;
+  @NonFinal @Setter int id;
+
+  @NonNull String email;
+  @NonNull String passwordHash;
+  @NonNull String firstName;
+  @NonNull String lastName;
+  @NonNull String birthDate;
 }
