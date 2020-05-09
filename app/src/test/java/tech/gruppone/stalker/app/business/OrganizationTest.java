1ee84f9535519879efbc74a72c02bf34f6d85594
tobiaapolloni <tobia.apolloni@gmail.com>
2020-05-09 11:33:15 +0200

    test: add tests for Organization class

--- app/src/test/java/tech/gruppone/stalker/app/business/OrganizationTest.java
+++ app/src/test/java/tech/gruppone/stalker/app/business/OrganizationTest.java
@@ -1,8 +1,11 @@
 package tech.gruppone.stalker.app.business;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static org.powermock.api.support.membermodification.MemberMatcher.method;
+import static org.powermock.api.support.membermodification.MemberModifier.stub;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,6 +13,8 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.Test;
+import org.mockito.Mockito;
+import tech.gruppone.stalker.app.utility.WebSingleton;
 
 public class OrganizationTest {
 
@@ -38,6 +43,46 @@ public class OrganizationTest {
     assertEquals(id, sut.getId());
     assertEquals(name, sut.getName());
     assertEquals(new ArrayList<Place>(), sut.getPlaces());
+  }
+
+  @Test
+  public void constructor_jsonPlaces_notNull() {
+    // Arrange
+    JSONObject jsonOrg = mock(JSONObject.class);
+    int id = 1;
+    String name = "unipd";
+    JSONArray jsonPlaces = mock(JSONArray.class);
+    JSONObject jsonPlace = mock(JSONObject.class);
+    JSONArray jsonPolyline = Mockito.mock(JSONArray.class);
+    JSONObject jsonPoint = Mockito.mock(JSONObject.class);
+
+    try {
+      when(jsonOrg.getInt("id")).thenReturn(id);
+      when(jsonOrg.getString("name")).thenReturn(name);
+      when(jsonOrg.getJSONArray("places")).thenReturn(jsonPlaces);
+
+      when(jsonPlaces.length()).thenReturn(1);
+      when(jsonPlaces.getJSONObject(0)).thenReturn(jsonPlace);
+
+      Mockito.when(jsonPlace.getInt("id")).thenReturn(id);
+      Mockito.when(jsonPlace.getJSONArray("polygon")).thenReturn(jsonPolyline);
+
+      Mockito.when(jsonPolyline.length()).thenReturn(1);
+      Mockito.when(jsonPolyline.getJSONObject(0)).thenReturn(jsonPoint);
+
+      Mockito.when(jsonPoint.getDouble("longitude")).thenReturn(0.0);
+      Mockito.when(jsonPoint.getDouble("latitude")).thenReturn(0.0);
+    } catch (JSONException e) {
+      throw new RuntimeException(e);
+    }
+
+    // Act
+    Organization sut = new Organization(jsonOrg);
+
+    // Assert
+    assertEquals(id, sut.getId());
+    assertEquals(name, sut.getName());
+    assertNotEquals(new ArrayList<Place>(), sut.getPlaces());
   }
 
   @Test
