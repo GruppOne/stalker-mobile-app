42d7224c0466cd90eaa0c7b368e47fdbefd500fe
tobiaapolloni <tobia.apolloni@gmail.com>
2020-05-10 18:25:08 +0200

    test: add tests for WebSingleton class

--- app/src/test/java/tech/gruppone/stalker/app/utility/WebSingletonTest.java
+++ app/src/test/java/tech/gruppone/stalker/app/utility/WebSingletonTest.java
@@ -2,11 +2,16 @@ package tech.gruppone.stalker.app.utility;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertSame;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.powermock.api.mockito.PowerMockito.method;
 import static org.powermock.api.mockito.PowerMockito.mock;
 import static org.powermock.api.mockito.PowerMockito.stub;
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
 import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.api.mockito.PowerMockito;
@@ -14,8 +19,11 @@ import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({WebSingleton.class})
+@PrepareForTest({WebSingleton.class, HeadersAdders.class})
 public class WebSingletonTest {
+
+  @Rule
+  InstantTaskExecutorRule taskExecutorRule = new InstantTaskExecutorRule();
 
   @Test
   public void getInstance_notNull() {
@@ -51,36 +59,59 @@ public class WebSingletonTest {
   @Test
   public void addToRequestQueue() {
     // Arrange
+    final RequestQueue queue = mock(RequestQueue.class);
+    stub(method(WebSingleton.class, "getRequestQueue")).toReturn(queue);
+
+    final WebSingleton sut = WebSingleton.getInstance();
+    final JsonObjectRequest req = mock(JsonObjectRequest.class);
 
     // Act
+    sut.addToRequestQueue(req);
 
     // Assert
+    verify(queue, times(1)).add(req);
   }
 
-  @Test
-  public void userEntered() {
-    // Arrange
+ /* @Test
+  public void getRequestQueue() throws Exception {
+    //Arrange
+    final
+    whenNew(WebSingleton.class).withNoArguments().thenReturn()
+    final RequestQueue queue = mock(RequestQueue.class);
+    stub(method(Volley.class, "newRequestQueue")).toReturn(queue);
 
-    // Act
+    final WebSingleton sut = WebSingleton.getInstance();
 
-    // Assert
-  }
+    //Act
+    final RequestQueue result = sut.getRequestQueue();
 
-  @Test
-  public void userExited() {
-    // Arrange
+    //Assert
+    assertEquals(result, queue);
+  }*/
 
-    // Act
+/*  @Test(expected=JSONException.class)
+  public void locationUpdateInside() throws Exception {
+    //Arrange
+    final RequestQueue queue = mock(RequestQueue.class);
+    final JsonObjectRequest request = mock(JsonObjectRequest.class);
+    final JSONObject jsonObject = mock(JSONObject.class);
 
-    // Assert
-  }
+    stub(method(WebSingleton.class, "getRequestQueue")).toReturn(queue);
+    stub(method(HeadersAdders.class, "buildObjReqWithHeaders")).toReturn(request);
+    //stub(method(JSONObject.class, "put")).toReturn(jsonObject);
 
-  @Test
-  public void getOrganizationList() {
-    // Arrange
+    when(jsonObject.put(anyString(), any(Object.class))).thenReturn(jsonObject);
+    whenNew(JSONObject.class).withAnyArguments().thenReturn(jsonObject);
 
-    // Act
+    final WebSingleton sut = WebSingleton.getInstance();
+    final List<Integer> intList = new ArrayList<>();
+    intList.add(1);
 
-    // Assert
-  }
+    //Act
+    sut.locationUpdateInside(1, intList);
+
+    //Assert
+    verify(queue, times(1)).add(request);
+  }*/
+
 }
