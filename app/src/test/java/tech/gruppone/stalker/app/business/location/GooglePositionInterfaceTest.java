61881206dfbe09b8da2629b79f8551b028c5a306
tobiaapolloni <tobia.apolloni@gmail.com>
2020-05-10 18:25:43 +0200

    test: add test class for GooglePositionInterface class

---
+++ app/src/test/java/tech/gruppone/stalker/app/business/location/GooglePositionInterfaceTest.java
@@ -0,0 +1,20 @@
+package tech.gruppone.stalker.app.business.location;
+
+import org.junit.Test;
+
+public class GooglePositionInterfaceTest {
+
+  @Test
+  public void startLocationUpdates() {
+  }
+
+  @Test
+  public void checkPermissions() {
+    //Arrange
+
+    //Act
+
+    //Assert
+
+  }
+}
