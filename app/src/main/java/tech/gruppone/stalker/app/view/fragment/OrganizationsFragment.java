b341e45eb2615c51b6eb4c54a634e1fd4634607a
Riccardo Agatea <riccardo.agatea@gmail.com>
2020-05-14 17:12:43 +0200

    fix: redistribute code between methods

--- app/src/main/java/tech/gruppone/stalker/app/view/fragment/OrganizationsFragment.java
+++ app/src/main/java/tech/gruppone/stalker/app/view/fragment/OrganizationsFragment.java
@@ -5,37 +5,29 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import tech.gruppone.stalker.app.R;
 import tech.gruppone.stalker.app.utility.OrganizationListAdapter;
-import tech.gruppone.stalker.app.view.MainPageActivity;
 import tech.gruppone.stalker.app.viewmodel.fragment.OrganizationsViewModel;
 
 public class OrganizationsFragment extends Fragment {
 
-  OrganizationsViewModel viewModel=new OrganizationsViewModel();
+  OrganizationsViewModel viewModel;
+  View view;
 
   public OrganizationsFragment() {
     // Required empty public constructor
   }
 
   @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
+  public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+    super.onActivityCreated(savedInstanceState);
 
     viewModel = new ViewModelProvider(this).get(OrganizationsViewModel.class);
-  }
-
-  @Override
-  public View onCreateView(
-      @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-    View view =inflater.inflate(R.layout.fragment_organizations, container, false);
-
-    view.findViewById(R.id.reloadButton)
-      .setOnClickListener(v -> OrganizationsFragment.this.loadOrganizations());
 
     RecyclerView recyclerView = view.findViewById(R.id.organizationRecyclerView);
     recyclerView.setHasFixedSize(true);
@@ -47,6 +39,15 @@ public class OrganizationsFragment extends Fragment {
     viewModel.getOrgsLiveData().observe(getViewLifecycleOwner(), adapter::submitList);
 
     recyclerView.setAdapter(adapter);
+  }
+
+  @Override
+  public View onCreateView(
+      @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    view = inflater.inflate(R.layout.organizations_fragment, container, false);
+
+    view.findViewById(R.id.reloadButton)
+        .setOnClickListener(v -> OrganizationsFragment.this.loadOrganizations());
 
     return view;
   }
