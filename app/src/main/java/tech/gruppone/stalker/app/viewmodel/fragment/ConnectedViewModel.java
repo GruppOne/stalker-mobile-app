07aa9ccbe247509c295716f12f79b8619369346c
Riccardo Agatea <riccardo.agatea@gmail.com>
2020-05-14 17:13:15 +0200

    feat: add fragment for connected list

---
+++ app/src/main/java/tech/gruppone/stalker/app/viewmodel/fragment/ConnectedViewModel.java
@@ -0,0 +1,7 @@
+package tech.gruppone.stalker.app.viewmodel.fragment;
+
+import androidx.lifecycle.ViewModel;
+
+public class ConnectedViewModel extends ViewModel {
+  // TODO: Implement the ViewModel
+}
