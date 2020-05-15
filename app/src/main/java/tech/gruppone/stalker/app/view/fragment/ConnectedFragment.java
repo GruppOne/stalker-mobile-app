b5c0206d379d632cbeb60a2ac72c521164e60acf
Riccardo Agatea <riccardo.agatea@gmail.com>
2020-05-15 17:01:01 +0200

    feat: implement list of connected orgs

--- app/src/main/java/tech/gruppone/stalker/app/view/fragment/ConnectedFragment.java
+++ app/src/main/java/tech/gruppone/stalker/app/view/fragment/ConnectedFragment.java
@@ -8,24 +8,42 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import tech.gruppone.stalker.app.R;
+import tech.gruppone.stalker.app.utility.OrganizationListAdapter;
 import tech.gruppone.stalker.app.viewmodel.fragment.ConnectedViewModel;
 
 public class ConnectedFragment extends Fragment {
 
   private ConnectedViewModel viewModel;
+  private View view;
 
   @Override
   public View onCreateView(
       @NonNull LayoutInflater inflater,
       @Nullable ViewGroup container,
       @Nullable Bundle savedInstanceState) {
-    return inflater.inflate(R.layout.connected_fragment, container, false);
+    view = inflater.inflate(R.layout.connected_fragment, container, false);
+
+    return view;
   }
 
   @Override
   public void onActivityCreated(@Nullable Bundle savedInstanceState) {
     super.onActivityCreated(savedInstanceState);
+
     viewModel = new ViewModelProvider(this).get(ConnectedViewModel.class);
+
+    RecyclerView recyclerView = view.findViewById(R.id.connectedRecyclerView);
+    recyclerView.setHasFixedSize(true);
+
+    recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+    OrganizationListAdapter adapter = new OrganizationListAdapter();
+
+    viewModel.getConnectedOrganizations().observe(getViewLifecycleOwner(), adapter::submitList);
+
+    recyclerView.setAdapter(adapter);
   }
 }
