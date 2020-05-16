12d6741b58ed7fc7a62774f8451a7154a4af7282
Fabio Scettro <fabioscettro@gmail.com>
2020-05-16 23:44:01 +0200

    feat: add splash page

--- app/src/main/java/tech/gruppone/stalker/app/view/MainActivity.java
+++ app/src/main/java/tech/gruppone/stalker/app/view/MainActivity.java
@@ -1,7 +1,9 @@
 package tech.gruppone.stalker.app.view;
 
+import android.content.Intent;
 import android.location.Location;
 import android.os.Bundle;
+import android.os.Handler;
 import tech.gruppone.stalker.app.R;
 import tech.gruppone.stalker.app.utility.StalkerActivity;
 
@@ -10,8 +12,25 @@ public class MainActivity extends StalkerActivity {
   private Location currentLocation;
 
   @Override
-  protected void onCreate(Bundle savedInstanceState) {
+  protected void onPause() {
+    super.onPause();
+    overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+  }
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    int secondsDelayed = 1;
+    new Handler()
+        .postDelayed(
+            new Runnable() {
+              public void run() {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                finish();
+              }
+            },
+            secondsDelayed * 900);
   }
 }
