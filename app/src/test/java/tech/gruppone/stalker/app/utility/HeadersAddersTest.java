0f98b9fbabc5ee9852c74960f36f15cca99e1594
tobiaapolloni <tobia.apolloni@gmail.com>
2020-05-24 19:19:41 +0200

    test: add test for HeaderAdders class

---
+++ app/src/test/java/tech/gruppone/stalker/app/utility/HeadersAddersTest.java
@@ -0,0 +1,48 @@
+package tech.gruppone.stalker.app.utility;
+
+import static org.junit.Assert.*;
+
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
+import java.util.Map;
+import org.json.JSONObject;
+import org.junit.Test;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+
+@PrepareForTest({})
+public class HeadersAddersTest {
+
+  @Test
+  public void getHeaders() {
+    //Arrange
+
+    //Act
+    Map<String, String> result = HeadersAdders.getHeaders();
+
+    //Assert
+    assertNotNull(result);
+  }
+
+  @Test
+  public void buildArrReqWithHeaders() {
+    //Arrange
+
+    //Act
+    JsonArrayRequest result = HeadersAdders.buildArrReqWithHeaders(1, "test", null, null, null);
+
+    //Assert
+    assertNotNull(result);
+  }
+
+  @Test
+  public void buildObjReqWithHeaders() {
+    //Arrange
+
+    //Act
+    JsonObjectRequest result = HeadersAdders.buildObjReqWithHeaders(1, "test", null, null, null);
+
+    //Assert
+    assertNotNull(result);
+  }
+}
