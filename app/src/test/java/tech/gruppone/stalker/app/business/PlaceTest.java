69677e3790d52a41c8c24bee9d4c788891051608
Riccardo Agatea <riccardo.agatea@gmail.com>
2020-05-28 16:00:14 +0200

    test: remove outdated test clsses

--- app/src/test/java/tech/gruppone/stalker/app/business/PlaceTest.java
+++ app/src/test/java/tech/gruppone/stalker/app/business/PlaceTest.java
@@ -83,4 +83,58 @@ public class PlaceTest {
     // Assert
     Assert.assertTrue(inside);
   }
+
+  @Test
+  public void isInside_insidePoint() {
+    // Arrange
+    final List<Pair<Double, Double>> pairList = new ArrayList<>();
+    pairList.add(new Pair<>(0.0, 0.0));
+    pairList.add(new Pair<>(1.0, 0.0));
+    pairList.add(new Pair<>(1.0, 1.0));
+    pairList.add(new Pair<>(0.0, 1.0));
+
+    final Place instance = new Place(pairList);
+
+    // Act
+    final boolean isInside = instance.isInside(new Pair<>(0.5, 0.5));
+
+    // Assert
+    assertTrue(isInside);
+  }
+
+  @Test
+  public void isInside_outsidePoint() {
+    // Arrange
+    final List<Pair<Double, Double>> pairList = new ArrayList<>();
+    pairList.add(new Pair<>(0.0, 0.0));
+    pairList.add(new Pair<>(1.0, 0.0));
+    pairList.add(new Pair<>(1.0, 1.0));
+    pairList.add(new Pair<>(0.0, 1.0));
+
+    final Place instance = new Place(pairList);
+
+    // Act
+    final boolean isInside = instance.isInside(new Pair<>(5.0, 5.0));
+
+    // Assert
+    assertFalse(isInside);
+  }
+
+  @Test
+  public void isInside_borderPoint() {
+    // Arrange
+    final List<Pair<Double, Double>> pairList = new ArrayList<>();
+    pairList.add(new Pair<>(0.0, 0.0));
+    pairList.add(new Pair<>(1.0, 0.0));
+    pairList.add(new Pair<>(1.0, 1.0));
+    pairList.add(new Pair<>(0.0, 1.0));
+
+    final Place instance = new Place(pairList);
+
+    // Act
+    final boolean isInside = instance.isInside(new Pair<>(0.0, 0.0));
+
+    // Assert
+    assertFalse(isInside);
+  }
 }
