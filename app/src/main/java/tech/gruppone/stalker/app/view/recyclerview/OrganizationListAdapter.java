127e77f160e48bea88479554df138f7817ba5551
Riccardo Agatea <riccardo.agatea@gmail.com>
2020-06-03 20:02:57 +0200

    feat: implement selection in lists

--- app/src/main/java/tech/gruppone/stalker/app/view/recyclerview/OrganizationListAdapter.java
+++ app/src/main/java/tech/gruppone/stalker/app/view/recyclerview/OrganizationListAdapter.java
@@ -1,4 +1,4 @@
-package tech.gruppone.stalker.app.utility;
+package tech.gruppone.stalker.app.view.recyclerview;
 
 import static java.util.Objects.requireNonNull;
 
@@ -11,13 +11,15 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
+import androidx.recyclerview.selection.SelectionTracker;
 import androidx.recyclerview.widget.DiffUtil.ItemCallback;
 import androidx.recyclerview.widget.ListAdapter;
 import androidx.recyclerview.widget.RecyclerView;
+import lombok.Setter;
 import tech.gruppone.stalker.app.R;
 import tech.gruppone.stalker.app.business.Organization;
-import tech.gruppone.stalker.app.utility.OrganizationListAdapter.OrgViewHolder;
 import tech.gruppone.stalker.app.view.OrganizationActivity;
+import tech.gruppone.stalker.app.view.recyclerview.OrganizationListAdapter.OrgViewHolder;
 
 public class OrganizationListAdapter extends ListAdapter<LiveData<Organization>, OrgViewHolder> {
 
@@ -31,8 +33,11 @@ public class OrganizationListAdapter extends ListAdapter<LiveData<Organization>,
 
     private Context context;
 
+    private View view;
+
     public OrgViewHolder(@NonNull View v) {
       super(v);
+      view = v;
       name = v.findViewById(R.id.organizationName);
       description = v.findViewById(R.id.organizationDescription);
       privatePublicIcon = v.findViewById(R.id.privatePublicIcon);
@@ -42,7 +47,7 @@ public class OrganizationListAdapter extends ListAdapter<LiveData<Organization>,
       context = v.getContext();
     }
 
-    public void bindTo(@NonNull LiveData<Organization> newOrganizationLiveData) {
+    public void bindTo(@NonNull LiveData<Organization> newOrganizationLiveData, boolean active) {
       Organization organization = requireNonNull(newOrganizationLiveData.getValue());
       id = organization.getId();
       name.setText(organization.getName());
@@ -54,6 +59,8 @@ public class OrganizationListAdapter extends ListAdapter<LiveData<Organization>,
           0,
           0,
           0);
+
+      view.setActivated(active);
     }
 
     @Override
@@ -63,6 +70,9 @@ public class OrganizationListAdapter extends ListAdapter<LiveData<Organization>,
       context.startActivity(intent);
     }
   }
+
+  @Setter
+  private SelectionTracker<Long> selectionTracker=null;
 
   public OrganizationListAdapter() {
     super(
@@ -80,6 +90,8 @@ public class OrganizationListAdapter extends ListAdapter<LiveData<Organization>,
             return requireNonNull(oldItem.getValue()).equals(newItem.getValue());
           }
         });
+
+    setHasStableIds(true);
   }
 
   @NonNull
@@ -91,11 +103,18 @@ public class OrganizationListAdapter extends ListAdapter<LiveData<Organization>,
 
   @Override
   public void onBindViewHolder(@NonNull OrgViewHolder holder, int position) {
-    holder.bindTo(getItem(position));
+    holder.bindTo(
+        getItem(position),
+        selectionTracker != null && selectionTracker.isSelected(holder.getItemId()));
   }
 
   @Override
   public int getItemViewType(int position) {
     return R.layout.organization_view;
+  }
+
+  @Override
+  public long getItemId(int position) {
+    return requireNonNull(getItem(position).getValue()).getId();
   }
 }
