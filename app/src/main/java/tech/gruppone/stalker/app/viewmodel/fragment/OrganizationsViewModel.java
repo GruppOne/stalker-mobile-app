513f122923b3ba189f5425a55027e2efe0571e45
Riccardo Agatea <riccardo.agatea@gmail.com>
2020-06-04 03:24:08 +0200

    feat: add support for multiple connections

--- app/src/main/java/tech/gruppone/stalker/app/viewmodel/fragment/OrganizationsViewModel.java
+++ app/src/main/java/tech/gruppone/stalker/app/viewmodel/fragment/OrganizationsViewModel.java
@@ -2,11 +2,14 @@ package tech.gruppone.stalker.app.viewmodel.fragment;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.Transformations;
 import androidx.lifecycle.ViewModel;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import tech.gruppone.stalker.app.business.Organization;
+import tech.gruppone.stalker.app.model.OrganizationModel;
 import tech.gruppone.stalker.app.model.fragment.OrganizationsModel;
 
 public class OrganizationsViewModel extends ViewModel {
@@ -20,5 +23,10 @@ public class OrganizationsViewModel extends ViewModel {
   @NonNull
   public LiveData<List<LiveData<Organization>>> getOrgsLiveData() {
     return Transformations.map(model.getOrgsLiveData(), input -> new ArrayList<>(input.values()));
+  }
+
+  public void connect(Iterator<Long> organizationIds) {
+    OrganizationModel organizationModel = new OrganizationModel();
+    organizationIds.forEachRemaining(id -> organizationModel.connect(id.intValue()));
   }
 }
