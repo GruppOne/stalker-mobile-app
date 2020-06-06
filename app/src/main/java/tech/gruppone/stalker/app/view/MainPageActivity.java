1c814cc383289b403d0c1a3421d5cfb9c4a0eae1
Riccardo Agatea <riccardo.agatea@gmail.com>
2020-06-06 20:01:39 +0200

    feat: implement anonymous toolbar activity

--- app/src/main/java/tech/gruppone/stalker/app/view/MainPageActivity.java
+++ app/src/main/java/tech/gruppone/stalker/app/view/MainPageActivity.java
@@ -1,24 +1,64 @@
 package tech.gruppone.stalker.app.view;
 
+import static java.util.Objects.requireNonNull;
+
 import android.os.Bundle;
+import android.view.Menu;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.ViewModelProvider;
+import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import java.util.Objects;
 import tech.gruppone.stalker.app.R;
+import tech.gruppone.stalker.app.utility.CurrentSessionSingleton;
 import tech.gruppone.stalker.app.utility.StalkerActivity;
 import tech.gruppone.stalker.app.utility.location.GooglePositionInterface;
+import tech.gruppone.stalker.app.viewmodel.MainPageViewModel;
 
 public class MainPageActivity extends StalkerActivity {
 
   FragmentManager fragmentManager = getSupportFragmentManager();
+
+  MainPageViewModel viewModel;
 
   @Override
   protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_mainpage);
 
-    setSupportActionBar(findViewById(R.id.topAppBarPages));
+    viewModel = new ViewModelProvider(this).get(MainPageViewModel.class);
+
+    MaterialToolbar topAppBar = findViewById(R.id.topAppBarPages);
+
+    setSupportActionBar(topAppBar);
+
+    requireNonNull(getSupportActionBar()).invalidateOptionsMenu();
+
+    topAppBar.getMenu().add("bubu");
+
+    requireNonNull(getSupportActionBar()).invalidateOptionsMenu();
+
+    topAppBar.setOnMenuItemClickListener(
+        item -> {
+          if (item.getItemId() == R.id.anonymousMenuItem) {
+            viewModel.toggleAnonymous();
+
+            boolean anonymous = CurrentSessionSingleton.getInstance().isAnonymous();
+
+            item.setIcon(
+                anonymous
+                    ? R.drawable.ic_visibility_black_24dp
+                    : R.drawable.ic_visibility_off_black_24dp);
+
+            item.setTitle(anonymous ? R.string.goKnown : R.string.goAnonymous);
+
+            item.setContentDescription(
+                getString(
+                    anonymous ? R.string.goKnownDescription : R.string.goAnonymousDescription));
+            return true;
+          }
+          return false;
+        });
 
     ((BottomNavigationView) findViewById(R.id.bottom_navigation))
         .setOnNavigationItemSelectedListener(
@@ -49,18 +89,24 @@ public class MainPageActivity extends StalkerActivity {
   private void setOrganizationsPage() {
     fragmentManager
         .beginTransaction()
-        .hide(Objects.requireNonNull(fragmentManager.findFragmentById(R.id.connected_fragment)))
-        .show(Objects.requireNonNull(fragmentManager.findFragmentById(R.id.organizations_fragment)))
+        .hide(requireNonNull(fragmentManager.findFragmentById(R.id.connected_fragment)))
+        .show(requireNonNull(fragmentManager.findFragmentById(R.id.organizations_fragment)))
         .commit();
   }
 
   private void setConnectedPage() {
     fragmentManager
         .beginTransaction()
-        .hide(Objects.requireNonNull(fragmentManager.findFragmentById(R.id.organizations_fragment)))
-        .show(Objects.requireNonNull(fragmentManager.findFragmentById(R.id.connected_fragment)))
+        .hide(requireNonNull(fragmentManager.findFragmentById(R.id.organizations_fragment)))
+        .show(requireNonNull(fragmentManager.findFragmentById(R.id.connected_fragment)))
         .commit();
   }
 
   private void setReportPage() {}
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.app_bar_menu, menu);
+    return true;
+  }
 }
--- app/src/main/java/tech/gruppone/stalker/app/viewmodel/MainPageViewModel.java
+++ app/src/main/java/tech/gruppone/stalker/app/viewmodel/MainPageViewModel.java
@@ -1,12 +1,12 @@
 package tech.gruppone.stalker.app.viewmodel;
 
 import androidx.lifecycle.ViewModel;
-import tech.gruppone.stalker.app.model.fragment.OrganizationsModel;
+import tech.gruppone.stalker.app.model.MainPageModel;
 
 public class MainPageViewModel extends ViewModel {
-  OrganizationsModel model = new OrganizationsModel();
+  private MainPageModel model = new MainPageModel();
 
-  public void loadOrganizations() {
-    model.loadOrganizations();
+  public void toggleAnonymous() {
+    model.toggleAnonymous();
   }
 }
