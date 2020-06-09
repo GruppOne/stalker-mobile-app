2e8813b12f9456f6551ff5db40f67d8e337405c6
Riccardo Agatea <riccardo.agatea@gmail.com>
2020-06-09 23:25:42 +0200

    fix: fix fragments misbehaving with screen rotation

--- app/src/main/java/tech/gruppone/stalker/app/viewmodel/MainPageViewModel.java
+++ app/src/main/java/tech/gruppone/stalker/app/viewmodel/MainPageViewModel.java
@@ -1,10 +1,18 @@
 package tech.gruppone.stalker.app.viewmodel;
 
+import androidx.annotation.IdRes;
 import androidx.lifecycle.ViewModel;
+import lombok.Getter;
+import lombok.Setter;
+import tech.gruppone.stalker.app.R;
 import tech.gruppone.stalker.app.model.MainPageModel;
 
 public class MainPageViewModel extends ViewModel {
-  private MainPageModel model = new MainPageModel();
+  private final MainPageModel model = new MainPageModel();
+  @Getter
+  @Setter
+  @IdRes
+  private int selectedMenuItemId = R.id.organizations_page;
 
   public void toggleAnonymous() {
     model.toggleAnonymous();
