11fda6a3f4ed57f866c5a502e7cc2bb855c1ca2a
Riccardo Agatea <riccardo.agatea@gmail.com>
2020-06-10 15:15:16 +0200

    style: format files

--- app/src/main/java/tech/gruppone/stalker/app/viewmodel/MainPageViewModel.java
+++ app/src/main/java/tech/gruppone/stalker/app/viewmodel/MainPageViewModel.java
@@ -9,10 +9,7 @@ import tech.gruppone.stalker.app.model.MainPageModel;
 
 public class MainPageViewModel extends ViewModel {
   private final MainPageModel model = new MainPageModel();
-  @Getter
-  @Setter
-  @IdRes
-  private int selectedMenuItemId = R.id.organizations_page;
+  @Getter @Setter @IdRes private int selectedMenuItemId = R.id.organizations_page;
 
   public void toggleAnonymous() {
     model.toggleAnonymous();
