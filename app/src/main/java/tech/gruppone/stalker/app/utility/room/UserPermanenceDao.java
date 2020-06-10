b17fa80bfacac03685d44cc2e9cce68f9fcda6d9
Riccardo Agatea <riccardo.agatea@gmail.com>
2020-06-10 22:37:30 +0200

    fix: fix entity and dao details

--- app/src/main/java/tech/gruppone/stalker/app/utility/room/UserPermanenceDao.java
+++ app/src/main/java/tech/gruppone/stalker/app/utility/room/UserPermanenceDao.java
@@ -3,21 +3,18 @@ package tech.gruppone.stalker.app.utility.room;
 import androidx.annotation.NonNull;
 import androidx.room.Dao;
 import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 import java.util.List;
 
 @Dao
 public interface UserPermanenceDao {
-  @Insert
+  @Insert(onConflict = OnConflictStrategy.IGNORE)
   void insert(@NonNull UserPermanence userPermanence);
 
   @Insert
   void update(@NonNull UserPermanence userPermanence);
 
-  @Query("SELECT COUNT(*) FROM userpermanence WHERE placeId = :placeId AND exitTimestamp = NULL")
-  int openEntries(int placeId);
-
-  @Query(
-      "SELECT placeId FROM userpermanence WHERE placeId NOT IN (:insideNow) AND exitTimestamp = NULL")
-  List<Integer> wasInside(@NonNull List<Integer> insideNow);
+  @Query("SELECT * FROM userpermanence WHERE placeId NOT IN (:insideNow) AND exitTimestamp = NULL")
+  List<UserPermanence> wasInside(@NonNull List<Integer> insideNow);
 }
