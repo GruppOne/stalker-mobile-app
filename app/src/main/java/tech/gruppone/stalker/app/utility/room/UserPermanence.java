7c4a7ed3e12c33abbbb23f4688feeadce4f2e941
Riccardo Agatea <riccardo.agatea@gmail.com>
2020-06-10 22:37:30 +0200

    fix: fix entity and dao details

--- app/src/main/java/tech/gruppone/stalker/app/utility/room/UserPermanence.java
+++ app/src/main/java/tech/gruppone/stalker/app/utility/room/UserPermanence.java
@@ -3,15 +3,18 @@ package tech.gruppone.stalker.app.utility.room;
 import androidx.annotation.NonNull;
 import androidx.room.Entity;
 import java.sql.Timestamp;
+import java.util.Date;
 import lombok.Builder;
 import lombok.Value;
+import lombok.With;
 
 @Value
 @Builder
 @Entity(primaryKeys = {"placeId", "entryTimestamp"})
 public class UserPermanence {
   int placeId;
-  @NonNull Timestamp entryTimestamp;
-  Timestamp exitTimestamp;
+  @NonNull
+  Date entryTimestamp;
+  @With Date exitTimestamp;
   boolean anonymous;
 }
