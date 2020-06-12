d0370e242006b56ac37537457379fb276b6e98ce
Riccardo Agatea <riccardo.agatea@gmail.com>
2020-06-12 19:34:01 +0200

    feat: add geofences broadcast reciever

---
+++ app/src/main/java/tech/gruppone/stalker/app/utility/location/GeofencesReceiver.java
@@ -0,0 +1,15 @@
+package tech.gruppone.stalker.app.utility.location;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class GeofencesReceiver extends BroadcastReceiver {
+
+  @Override
+  public void onReceive(Context context, Intent intent) {
+    // TODO: This method is called when the BroadcastReceiver is receiving
+    // an Intent broadcast.
+    throw new UnsupportedOperationException("Not yet implemented");
+  }
+}
