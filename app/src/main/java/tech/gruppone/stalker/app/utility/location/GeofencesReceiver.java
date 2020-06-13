4c449343efd9b8fb1c3c1280f25315cc27b95fea
Riccardo Agatea <riccardo.agatea@gmail.com>
2020-06-13 19:16:04 +0200

    feat: implement geofence triggering receiver

--- app/src/main/java/tech/gruppone/stalker/app/utility/location/GeofencesReceiver.java
+++ app/src/main/java/tech/gruppone/stalker/app/utility/location/GeofencesReceiver.java
@@ -3,13 +3,51 @@ package tech.gruppone.stalker.app.utility.location;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingEvent;
+import java.util.List;
+import java.util.Vector;
+import java.util.stream.Collectors;
 
 public class GeofencesReceiver extends BroadcastReceiver {
 
+  private final List<Integer> nearPlaceIds = new Vector<>();
+  private final GooglePositionInterface googlePositionInterface = new GooglePositionInterface();
+
   @Override
   public void onReceive(Context context, Intent intent) {
-    // TODO: This method is called when the BroadcastReceiver is receiving
-    // an Intent broadcast.
-    throw new UnsupportedOperationException("Not yet implemented");
+    GeofencingEvent geofencingEvent = GeofencingEvent.fromIntent(intent);
+
+    if (geofencingEvent.hasError()) {
+      return;
+    }
+
+    int transitionType = geofencingEvent.getGeofenceTransition();
+
+    if (transitionType == Geofence.GEOFENCE_TRANSITION_DWELL) {
+      if (nearPlaceIds.isEmpty()) {
+        googlePositionInterface.startLocationUpdates();
+      }
+
+      nearPlaceIds.addAll(
+          geofencingEvent
+              .getTriggeringGeofences()
+              .parallelStream()
+              .map(Geofence::getRequestId)
+              .map(Integer::parseInt)
+              .collect(Collectors.toList()));
+    } else if (transitionType == Geofence.GEOFENCE_TRANSITION_EXIT) {
+      nearPlaceIds.removeAll(
+          geofencingEvent
+              .getTriggeringGeofences()
+              .parallelStream()
+              .map(Geofence::getRequestId)
+              .map(Integer::parseInt)
+              .collect(Collectors.toList()));
+
+      if (nearPlaceIds.isEmpty()) {
+        googlePositionInterface.stopLocationUpdates();
+      }
+    }
   }
 }
